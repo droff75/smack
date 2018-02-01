8ccaaa8094316643c96971a6fc5d3cfc9a856491
David Roff <David.Roff@asynchrony.com>
2018-02-01 12:26:40 -0600

    added login view

---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by david.roff on 2/1/18.
+//  Copyright © 2018 david.roff. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"

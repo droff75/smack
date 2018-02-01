da37b282d3de5f70649b080c89b1e2cd6b36c7b6
David Roff <David.Roff@asynchrony.com>
2018-02-01 12:52:57 -0600

    added create profile view

---
+++ Smack/Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,23 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by david.roff on 2/1/18.
+//  Copyright © 2018 david.roff. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+
+}

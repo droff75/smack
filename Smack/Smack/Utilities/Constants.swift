2b94e76af1728d67943da6eb5af33cb69bfda128
David Roff <David.Roff@asynchrony.com>
2018-02-02 13:52:10 -0600

    added authservice

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -8,6 +8,15 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+let BASE_URL = "https://droffsmackapp.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"

bd20d42b259005a29ec7114c3309f446650ddea7
David Roff <David.Roff@asynchrony.com>
2018-02-06 14:36:10 -0600

    added login functionality

--- Smack/Smack/Controller/LoginVC.swift
+++ Smack/Smack/Controller/LoginVC.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var userNameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUpView()
     }
 
     @IBAction func closePressed(_ sender: Any) {
@@ -23,4 +27,30 @@ class LoginVC: UIViewController {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
     
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = userNameTxt.text , userNameTxt.text != "" else { return }
+        guard let pass = passwordTxt.text , passwordTxt.text != "" else { return }
+        
+        AuthService.instance.loginUser(email: email, password: pass) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
+    }
+    
+    func setUpView() {
+        spinner.isHidden = true
+        userNameTxt.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+    }
 }

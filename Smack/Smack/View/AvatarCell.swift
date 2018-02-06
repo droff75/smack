c3349a117967b2c9f4a61c42a472419813d7152c
David Roff <David.Roff@asynchrony.com>
2018-02-06 09:25:26 -0600

    added avatar picker vc

---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,25 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by david.roff on 2/6/18.
+//  Copyright © 2018 david.roff. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}

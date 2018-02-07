ce658c232fd92a5b0396612eee23c5f16d500bf3
David Roff <David.Roff@asynchrony.com>
2018-02-07 14:48:32 -0600

    added ability to get messages by channel from service

---
+++ Smack/Smack/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by david.roff on 2/7/18.
+//  Copyright © 2018 david.roff. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}

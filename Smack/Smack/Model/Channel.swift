42452b4505422258414ccf3ef0312d896589e2b9
David Roff <David.Roff@asynchrony.com>
2018-02-07 08:57:58 -0600

    added message service

---
+++ Smack/Smack/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by david.roff on 2/7/18.
+//  Copyright © 2018 david.roff. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
